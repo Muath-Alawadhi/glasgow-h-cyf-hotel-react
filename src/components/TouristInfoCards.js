a2ec813a221f6fe303cc375d87685a7a22fe4429
Muath Alawadhi <109815858+Muath-Alawadhi@users.noreply.github.com>
2023-06-24 11:35:22 +0100

    Revert "Merge pull request #18 from Muath-Alawadhi/for-review"
    This reverts commit 2b436bec6809aeaf4838fe7e6f35d4210a4711b6, reversing
    changes made to 79733fde2f331e078bfb42c89cd39c98efd0fffc.

--- src/components/TouristInfoCards.js
+++ src/components/TouristInfoCards.js
@@ -1,26 +1,16 @@
-import cities from "../data/cities.json";
+import React from 'react'
+import GlasgowCards from './GlasgowCards-TouristInfoCards'
+import ManchesterCards from './ManchesterCards-TouristInfoCards'
+import LondonCards from './LondonCard-TouristInfoCards'
 
 const TouristInfoCards = () => {
-  const prop = cities;
   return (
-    <div className="Main-container">
-      {prop.map((card) => (
-        <div className="cards-container">
-          <div className="card">
-            <img src={card.imgSrc} alt={card.City} className="card-img-top" />
-          </div>
-
-          <div className="card-body">
-            <h1>{card.City}</h1>
-            <p>{card.description}</p>
-            <a href={card.link} target="_blank" className="btn btn-primary">
-              Visit Website
-            </a>
-          </div>
-        </div>
-      ))}
+    <div className="cards-container">
+      <GlasgowCards />
+      <ManchesterCards />
+      <LondonCards />
     </div>
-  );
-};
+  )
+}
 
-export default TouristInfoCards;
+export default TouristInfoCards
